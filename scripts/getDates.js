6916882c4a9bbba47505ee73c9ec05b6292198ec
natafran <natalufranceschi@gmail.com>
2023-01-06 21:05:00 -0800

    update

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
+
+var dt = new Date();
+document.getElementById("copyrightYear").innerHTML= dt.getFullYear(); 
