0ebd61711d078d693c297f7440699ae3d80cbac2
natafran <natalufranceschi@gmail.com>
2023-01-12 18:08:36 -0800

    hamburger menu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,18 @@ document.getElementById("lastModified").innerHTML = text;
 
 var dt = new Date();
 document.getElementById("copyrightYear").innerHTML= dt.getFullYear(); 
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+
+}))
